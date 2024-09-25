655fd74a2414386b8aeaf81820a850255cdd9504
Darwin Luque <50032250+darwin-luque@users.noreply.github.com>
2024-09-25 11:08:56 -0600

    feat: seelct pdfs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { PDFSelector } from "@/components/pdf-selector";
+import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
+
 function App() {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onSelectFiles = (files: FileList) =>
+    setFiles((prevFiles) => [...prevFiles, ...Array.from(files)]);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-screen h-screen flex flex-col gap-4 px-6 py-4">
+      <h1 className="font-semibold text-xl">PDF Annotation Proof of Concept</h1>
+      <Separator />
+      <div className="grid grid-cols-3 my-4 h-full">
+        <div className="col-span-1 flex w-full h-full">
+          <div className="flex-1">
+            <PDFSelector onSelected={onSelectFiles} />
+            <ul className="mt-4">
+              {files.map((file, index) => (
+                <li key={index}>{file.name}</li>
+              ))}
+            </ul>
+          </div>
+          <Separator orientation="vertical" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/pdf-selector/pdf-selector.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FC, useRef } from "react";
+
+export type PDFSelectorProps = {
+  onSelected: (files: FileList) => void;
+};
+
+export const PDFSelector: FC<PDFSelectorProps> = ({ onSelected }) => {
+  const pdfSelectorRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Select a PDF</Label>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input
+          placeholder="Pdf Input"
+          type="file"
+          accept=".pdf"
+          multiple
+          ref={pdfSelectorRef}
+        />
+        <Button
+          onClick={() => {
+            const files = pdfSelectorRef.current?.files;
+            if (files) onSelected(files);
+          }}
+          type="submit"
+        >
+          Select
+        </Button>
+      </div>
+    </div>
+  );
+};
